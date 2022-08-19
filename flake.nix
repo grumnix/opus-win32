c2ee58f8908bcc677ed854c548d3f662293e1cac
Ingo Ruhnke <grumbel@gmail.com>
2022-08-19 17:44:55 +0200

    Remove flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -1,17 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
-    tinycmmc.inputs.flake-utils.follows = "flake-utils";
 
     opus_src.url = "https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz";
     opus_src.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, tinycmmc, opus_src }:
+  outputs = { self, nixpkgs, tinycmmc, opus_src }:
     tinycmmc.lib.eachWin32SystemWithPkgs (pkgs:
       {
         packages = rec {
